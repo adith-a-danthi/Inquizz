13ad79efff9b230c69a4c455b03dcd6e58761725
Adith A Danthi <adith.danthi@gmail.com>
2022-05-15 23:59:47 +0530

    feat: update Navbar link

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,13 @@
+import { Link } from 'react-router-dom';
+
 export default function Navbar() {
   return (
     <nav className="navbar">
       <h3 className="nav-title heading-2">
-        <span className="fas fa-bolt fa-xs mr-2"></span>
-        Inquizz
+        <Link to="/">
+          <span className="fas fa-bolt fa-xs mr-2"></span>
+          Inquizz
+        </Link>
       </h3>
     </nav>
   );
