e339b0fa78cb2508143184227ae0d413b13bd108
Adith A Danthi <adith.danthi@gmail.com>
2022-05-15 23:47:27 +0530

    feat: add Navbar component

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,10 @@
+export default function Navbar() {
+  return (
+    <nav className="navbar">
+      <h3 className="nav-title heading-2">
+        <span className="fas fa-bolt fa-xs mr-2"></span>
+        Inquizz
+      </h3>
+    </nav>
+  );
+}
